d7c3fcc9dcaa6c44aaa2a97f0c1d5f68d98f44b1
phontso <phontsotsetan@gmail.com>
2019-01-24 19:47:26 -0700

    second commit

--- shopper-start/js/app.js
+++ shopper-start/js/app.js
@@ -1,7 +1,7 @@
 Vue.component('song',{
 
 	template: `
-			<li class=""><slot></slot></li>
+			<li class="list-group-item list-group-item-success"><slot></slot></li>
 	`
 
 });
@@ -14,21 +14,21 @@ Vue.component('song-list', {
 					<div class="form-group">
 					<label for="exampleInputEmail1">Song Name</label>
 
-					<input type="text" class="form-control" id="song"  placeholder="Song Name">
+					<input type="text" v-model="newItem.title" class="form-control" id="song"  placeholder="Song Name">
 
 					</div>
 					<div class="form-group">
 					<label for="exampleInputPassword1">Artist</label>
-					<input type="text" class="form-control" id="song" placeholder="Artist">
+					<input type="text" v-model="newItem.artest" class="form-control" id="song" placeholder="Artist">
 					</div>
 
-					<button type="submit" class="btn btn-primary">Add Song to the playlist </button>
+					<button @click="addItem" class="btn btn-primary">Add Song to the playlist </button>
 
 
 					<h4 class ="mt-5">My Playlist</h4>
-					<ul v-for="song in songs">
-					<song>{{ song.title }}</song>
-					<song>{{ song.artest }}</song>
+					<ul list-group v-for="song in songs">
+					<song>Title {{ song.title }}</song>
+					<song>Artist {{ song.artest }}</song>
 					</ul>
 				</div>
 				`,
@@ -36,29 +36,50 @@ Vue.component('song-list', {
 
 						data(){
 							return {
-								newItem:'',
+								newItem:[
+									{
+										title:'',
+										artest:''
+									}
+
+								],
 					 		
 					 			songs:[
 						 			 {
-						 			 	title:"apple",
-						 			 	artest:"orange"
+						 			 	title:"ROCKSTAR",
+						 			 	artest:"POST MALONE FT 21 SAVAGE"
 						 			 },
 						 			 {
-						 			 	title:"apple",
-						 			 	artest:"orange"
+						 			 	title:"	A MILLION DREAMS",
+						 			 	artest:"ZIV ZAIFMAN/JACKMAN/WILLIAMS"
 						 			 },
 						 			  {
-						 			 	title:"apple",
-						 			 	artest:"orange"
+						 			 	title:"	I LIKE IT",
+						 			 	artest:"CARDI B/BAD BUNNY/J BALVIN"
 						 			 }
 
 
 					 			
 					 			]
 								
-							
 						}
-					}
+					},
+
+  
+ 	methods:{
+ 			addItem(){
+
+ 				this.songs.push(this.newItem);
+ 				this.newItem ='';
+
+ 			}
+
+ 	},
+
+
+
+
+
 
 
 
