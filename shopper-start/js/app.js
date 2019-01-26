710aba5c64615cc3f052ad8950070858360a5b71
phontso <phontsotsetan@gmail.com>
2019-01-26 11:25:41 -0700

    first commit

--- shopper-start/js/app.js
+++ shopper-start/js/app.js
@@ -1,92 +1,87 @@
-Vue.component('song',{
+var songs = Vue.component('song',{
+
+	props: ['song'],
 
 	template: `
-			<li class="list-group-item list-group-item-success"><slot></slot></li>
-	`
+	<li class="list-group-item list-group-item-success">
 
+	    <h4>{{song.title}}</h4>
+        <small>{{song.artest}}</small>
+	</li>
+	`
 });
 
 
-Vue.component('song-list', {
-				template: `
-					<div>
-							
-					<div class="form-group">
-					<label for="exampleInputEmail1">Song Name</label>
-
-					<input type="text" v-model="newItem.title" class="form-control" id="song"  placeholder="Song Name">
-
-					</div>
-					<div class="form-group">
-					<label for="exampleInputPassword1">Artist</label>
-					<input type="text" v-model="newItem.artest" class="form-control" id="song" placeholder="Artist">
-					</div>
-
-					<button @click="addItem" class="btn btn-primary">Add Song to the playlist </button>
+var playList= Vue.component('song-list', {
+	template: `
+	<div>
+	
+		<div class="form-group">
+			<label for="exampleInputEmail1">Song Name</label>
+			<input type="text" v-model="newItem.title" class="form-control"   placeholder="Song Name">
+			</div>
+			<div class="form-group">
+			<label for="exampleInputPassword1">Artist</label>
+			<input type="text" v-model="newItem.artest" class="form-control" id="song" placeholder="Artist">
+		</div>
+		<button @click="addItem" class="btn btn-primary">Add Song to the playlist </button>
+		<h4 class ="mt-5">My Playlist</h4>
 
 
-					<h4 class ="mt-5">My Playlist</h4>
-					<ul list-group v-for="song in songs">
-					<song>Title {{ song.title }}</song>
-					<song>Artist {{ song.artest }}</song>
+				<ul class="list-group">
 
+					<song 
+
+					v-for="song in songs"
+
+					:song="song">
+
+					</song>
 					
-					</ul>
-				</div>
-				`,
 
-
-						data(){
-							return {
-								newItem:[
-									{
-										title:'',
-										artest:''
-									}
-
-								],
-					 		
-					 			songs:[
-						 			 {
-						 			 	title:"ROCKSTAR",
-						 			 	artest:"POST MALONE FT 21 SAVAGE"
-						 			 },
-						 			 {
-						 			 	title:"	A MILLION DREAMS",
-						 			 	artest:"ZIV ZAIFMAN/JACKMAN/WILLIAMS"
-						 			 },
-						 			  {
-						 			 	title:"	I LIKE IT",
-						 			 	artest:"CARDI B/BAD BUNNY/J BALVIN"
-						 			 }
-
-
-					 			
-					 			]
-								
-						}
-					},
-
-  
- 	methods:{
- 			addItem(){
-
- 				this.songs.push(this.newItem);
- 				this.addItem ='';
-
- 			}
-
- 	},
+				</ul>
+	</div>
+	`,
 
 
 
+	data(){
+		return {
+			newItem:
+			{
+				title:'',
+				artest:''
+			},
 
+			songs:[
+			{
+				title:"ROCKSTAR",
+				artest:"POST MALONE FT 21 SAVAGE"
+			},
+			{
+				title:"	A MILLION DREAMS",
+				artest:"ZIV ZAIFMAN/JACKMAN/WILLIAMS"
+			},
+			{
+				title:"	I LIKE IT",
+				artest:"CARDI B/BAD BUNNY/J BALVIN"
+			}
 
+			]
 
+		}
+	},
 
-
-
-
+	methods:{
+		addItem(){
+			this.songs.push(this.newItem);
+			this.newItem=
+			{
+				title:'',
+				artest:''
+			}
+		}
+	},
 });
 
 
@@ -97,8 +92,7 @@ Vue.component('song-list', {
 
 
 var app4 = new Vue({
-    el:'#demo-4',
-        	
+	el:'#demo-4',
 
-    	
+
 });
